7d0a3e439e41d4e1f63a5d7fd019410306c38140
ge-arty <george.arutinyan@gmail.com>
2023-04-24 14:02:19 +0400

    lastPage-markUp-update

--- src/components/FinalStage.jsx
+++ src/components/FinalStage.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import "./FinalStage.css";
 
 export default function FinalStage() {
-  return <div>3</div>;
+  return (
+    <div className="finalstage-container">
+      <div className="finalstage-header">
+        <div className="finalstage-header-banner"></div>
+        <h3 className="finalstage-header-title">
+          ᲒᲐᲔᲛᲒᲖᲐᲕᲠᲔ THE FESTIVAL IN MALTA-ᲖᲔ!
+        </h3>
+        <p className="finalstage-header-text">
+          The Festival სერიების ფარგლებში, ევროპაბეთი უკვე მეორედ აძლევს პოკერის
+          მოთამაშეებს დაუვიწყარი მოგზაურობის შანსს.
+        </p>
+        <div className="finalstage-header-date-box">
+          <div></div>
+          <p>15-დან 21მაისის ჩათვლით მალტა პოკერის ფესტივალს უმასპინძლებს</p>
+        </div>
+      </div>
+      <div className="finalstage-cards">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="finalstage-prize-cards"></div>
+      <div className="finalstage-mistery-key-container"></div>
+    </div>
+  );
 }
